67b3165a8847b5aa446eb69b72f31298e9771a47
soumen-gif <soumentaki1996@gmail.com>
2020-09-01 19:36:47 +0530

    Added: Extend the method to take more than three input

--- src/main/java/com/bridgelabz/generic/FindMaxValue.java
+++ src/main/java/com/bridgelabz/generic/FindMaxValue.java
@@ -1,15 +1,40 @@
 package com.bridgelabz.generic;
 
-public class FindMaxValue {
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
-        public  <Generic extends Comparable >Generic findMaximumValue(Generic arr[])
-        {
-            Generic max = arr[0];
-            for (int i =0 ; i< arr.length ; i++)
-            {
-                if( max.compareTo(arr[i]) <0 )
-                    max = arr[i];
-            }
-            return max;
+public class FindMaxValue <E extends Comparable <E>>{
+    E[] values;
+    //List of Generic Values
+    List<E> listValues = new LinkedList<>();
+
+    //Constructor to Initialize Generic Array
+    public FindMaxValue(E[] values) {
+        this.values = values;
+    }
+
+    //Constructor to Initialize Generic List
+    public FindMaxValue(List<E> listValues) {
+        this.listValues = listValues;
+    }
+
+    //Method to Find  Max Value From List
+    public E findMax() {
+        return Collections.max(listValues);
+    }
+
+    //Method to Find Max Value Form Array
+    public E findMaxValue() {
+        if (values[0].compareTo(values[1]) > 0 && values[0].compareTo(values[2]) > 0) {
+            return values[0];
+        } else if (values[1].compareTo(values[0]) > 0 && values[1].compareTo(values[2]) > 0) {
+            return values[1];
+        } else {
+            return values[2];
         }
+    }
+
 }
+
+
--- src/test/java/com/bridgelabz/generic/FindMaxValueTest.java
+++ src/test/java/com/bridgelabz/generic/FindMaxValueTest.java
@@ -1,71 +1,30 @@
 package com.bridgelabz.generic;
-
+import com.bridgelabz.generic.FindMaxValue;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
+import static java.lang.StrictMath.E;
 public class FindMaxValueTest {
-    FindMaxValue findMaxValue=null;
-    @Before
-    public void createObject()
-    {
-         findMaxValue= new FindMaxValue();
-    }
     @Test
-    public void whenGivenNoInArray_InFirstPosition_Shold_ReturnSameNumber()
-    {
-        Integer arr[] = {10,5,2};
-        Assert.assertSame(10,findMaxValue.findMaximumValue(arr));
-    }
-    @Test
-    public void whenGivenNoInArray_InSecondPosition_Should_ReturnSameNumber()
-    {
-        Integer arr[] = {5,10,2};
-        Assert.assertSame(10,findMaxValue.findMaximumValue(arr));
-    }
-    @Test
-    public void whenGivenNoInArray_InThirdPosition_Should_ReturnSameNumber()
-    {
-        Integer arr[] = {2,5,10};
-        Assert.assertSame(10,findMaxValue.findMaximumValue(arr));
-    }
-    @Test
-    public void whenGivenFloatNoInArray_InFirstPosition_Should_ReturnSameNumber()
-    {
-        Float arr[] = {10.5f,5.5f,2.5f};
-        Assert.assertEquals(10.5f,findMaxValue.findMaximumValue(arr),0.0);
-    }
-    @Test
-    public void whenGivenFloatNoInArray_InSecondPosition_Should_ReturnSameNumber()
-    {
-        Float arr[] = {5.5f,10.5f,2.5f};
-        Assert.assertEquals(10.5f,findMaxValue.findMaximumValue(arr),0.0);
-    }
-    @Test
-    public void whenGivenFloatNoInArray_InThirdPosition_Should_ReturnSameNumber()
-    {
-
-        Float arr[] = {2.5f,5.5f,10.5f};
-        Assert.assertEquals(10.5,findMaxValue.findMaximumValue(arr),0.0);
+    public void whenGiven_No_InFirstPosition_should_ReturnSameNo() {
+        Integer[] integerValues = {10, 5, 2};
+        Integer max = new FindMaxValue<>(integerValues).findMaxValue();
+        Assert.assertEquals((Integer) 10, max);
     }
 
     @Test
-    public void whenGivenStringInArray_InFirstPosition_Should_ReturnSameString()
-    {
-        String arr[] = {"watermelon" , "apple"  , "banana"};
-        Assert.assertEquals("watermelon",findMaxValue.findMaximumValue(arr));
+    public void whenGiven_No_InSecondPosition_should_ReturnSameNo() {
+        Integer[] integerValues = {5, 10, 2};
+        Integer max = new FindMaxValue<>(integerValues).findMaxValue();
+        Assert.assertEquals((Integer) 10, max);
     }
+
     @Test
-    public void whenGivenStringInArray_InSecondPosition_thenReturnSameString()
-    {
-        String arr[] = { "apple"  , "watermelon" , "banana"};
-        Assert.assertEquals("watermelon",findMaxValue.findMaximumValue(arr));
+    public void whenGiven_No_InThirdPosition_should_ReturnSameNo() {
+        Integer[] integerValues = {2, 5, 10};
+        Integer max = new FindMaxValue<>(integerValues).findMaxValue();
+        Assert.assertEquals((Integer) 10, max);
     }
-    @Test
-    public void whenGivenString_InThirdPosition_thenReturnSameString()
-    {
-        String arr[] = { "apple"  , "banana" , "watermelon"};
-        Assert.assertEquals("watermelon",findMaxValue.findMaximumValue(arr));
-    }
+
+
 }
-
